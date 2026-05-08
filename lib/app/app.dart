6b772d8be49a11767eabb429dc4a94d9277c8e76
mdefa <mdefaj50@gmail.com>
2026-05-09 04:27:24 +0600

    add firebase Analytics & Crashlytics

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,11 +1,23 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 
-class eStore extends StatelessWidget {
+class eStore extends StatefulWidget {
   const eStore({super.key});
+
+  @override
+  State<eStore> createState() => _eStoreState();
+}
+
+class _eStoreState extends State<eStore> {
+  static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  static FirebaseAnalyticsObserver observer = FirebaseAnalyticsObserver(analytics: analytics);
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorObservers: <NavigatorObserver>[observer],
+      debugShowCheckedModeBanner: true,
       home: Scaffold(),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
-import 'package:flutter/cupertino.dart';
-import 'app/app.dart';
+import 'dart:ui';
 
-main(){
-  runApp(const eStore());
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
+import 'app/app.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+// Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+  runApp(eStore());
 }
